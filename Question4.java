ccf8837a06eaa24116554b14859d2c8b29a72d90
sriman <102974631+sriman7849@users.noreply.github.com>
2022-07-27 13:13:59 +0530

    Add files via upload

---
+++ Question4.java
@@ -0,0 +1,20 @@
+
+public class Question4 {
+	public static void main(String[] args) {
+		
+		int n=14;
+		
+		for(int i=0;i<=n;++i){
+			
+		    for(int j=0;j<=n;++j){
+		    	
+		    	if( i==n ||i-j>=(n-1)/2 || i+j>=n+(n-1)/2)
+		    		System.out.print("*");
+		    	else
+		    		System.out.print(" ");
+		    }
+		    
+		    System.out.println();
+		}
+	}
+}
